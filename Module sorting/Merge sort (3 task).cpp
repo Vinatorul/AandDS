49eb9519755a22af5fc30165a3e5fe527fcb7c5d
Alexander Kuvaev <alexander@kuvaev.me>
2020-03-01 21:51:04 +0300

    Module sorting

---
+++ Module sorting/Merge sort (3 task).cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> merge(vector<int> &A, vector<int> &B) {
+    int i = 0;
+    int j = 0;
+    vector<int> C(A.size() + B.size());
+    for (int k = 0; k < C.size(); k++) {
+        // Если в массиве А все элементы закончились
+        if (i == A.size()) {
+            C[k] = B[j];
+            j++;
+        // Если в массиве B все элементы закончились
+        } else if (j == B.size()) {
+            C[k] = A[i];
+            i++;
+        } else if (A[i] <= B[j]) {
+            C[k] = A[i];
+            i++;
+        } else {
+            C[k] = B[j];
+            j++;
+        }
+    }
+    return C;
+}
+
+vector<int> merge_sort(vector<int> &V, int l, int r) {
+    // Проверяем, не равна ли длина 1
+    // Частный случай, при котором рекурсия завершается
+    if (r - l == 1) {
+        vector<int> res(1);
+        res[0] = V[l];
+        return res;
+    }
+    // Находим середину массива
+    int m = (l + r) / 2;
+    // Сортируем левую и правую половины независимо
+    vector<int> left = merge_sort(V, l, m);
+    vector<int> right = merge_sort(V, m, r);
+    // Сливаем отсортированные половины
+    return merge(left, right);
+}
+
+int main()
+{
+    // Чтение данных
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    // Вызов сортировки
+    vector<int> v_sorted = merge_sort(v, 0, n);
+    // Вывод результата
+    for (int i = 0; i < n; i++) {
+        cout << v_sorted[i] << ' ';
+    }
+    return 0;
+}
---
+++ Module sorting/Quick sort (4 task).cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &A, int l, int r) {
+    // Инициализация
+    int i = l;
+    int j = r - 1;
+    int v = A[(l + r - 1) / 2];
+    // Запускаем цикл, пока индексы не встретятся
+    while (i <= j) {
+        // Двигаем индекс i, пока A[i] упорядочены относительно разделяющего элемента
+        while (A[i] < v)
+            i++;
+        // Двигаем индекс j, пока A[j] упорядочены относительно разделяющего элемента
+        while (A[j] > v)
+            j--;
+        // Если индексы встретелись - выходим
+        if (i >= j)
+            break;
+        // Если мы дошли до сюда, то A[i] и A[j] стоят не на своих местах, меняем
+        swap(A[i], A[j]);
+        i++;
+        j--;
+    }
+    // Возвращаем индекс по которому произошло разделение
+    return j + 1;
+}
+
+void quicksort(vector<int> &A, int l, int r) {
+    // Если полуинтервал имеет длину меньше 2, то сортировать нечего
+    if (r - l <= 1) {
+        return;
+    }
+    // Разбиваем массив на два полуинтервала
+    int q = partition(A, l, r);
+    // Сортируем два новых полуинтервала независимо
+    quicksort(A, l, q);
+    quicksort(A, q, r);
+}
+
+int main() {
+    // Ввод данных
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    quicksort(v, 0, n);
+    for (int i = 0; i < n; ++i) {
+        cout << v[i] << ' ';
+    }
+    return 0;
+}
